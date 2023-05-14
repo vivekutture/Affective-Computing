3bc6a4d6924455baeb9a631a2fcba7b655266a00
Vivek Utture <uttureviveks@gmail.com>
2023-05-14 22:00:51 +0530

    Updated gitignore and SER-NLP

--- SER-NLP/SER-NLP.py
+++ SER-NLP/SER-NLP.py
@@ -14,6 +14,13 @@ model = pickle.load(open("SER-Trained-Model.model", "rb"))
 
 filename = "test.wav"
 
+if os.path.exists(filename):
+  os.remove(filename)
+
+if os.path.exists("read.txt"):
+  os.remove("read.txt")
+
+
 # record the file (start talking)
 
 r = sr.Recognizer()
@@ -32,18 +39,20 @@ with sr.Microphone() as source:
     print("Error : Voice not Recorded!!! ")
 
 
-try:
-  print("\nPrediciting Emotion...")
-  # extract features and reshape it
-  features = extract_feature(filename, mfcc=True, chroma=True, mel=True).reshape(1, -1)
-  # predict
-  result = model.predict(features)[0]
-  # show the result
-  print("\n")
-  print("Sentiment by SER : ", result.capitalize())
-  print("\n")
-  NLP()
-  print("Emotion Predicted Successfully!")
-  print("\n")
-except Exception as e:
+if os.path.exists(filename) and os.path.exists("read.txt"):
+  try:
+    print("\nPrediciting Emotion...")
+    # extract features and reshape it
+    features = extract_feature(filename, mfcc=True, chroma=True, mel=True).reshape(1, -1)
+    # predict
+    result = model.predict(features)[0]
+    # show the result
+    print("\n")
+    print("Sentiment by SER : ", result.capitalize())
+    print("\n")
+    NLP()
+    print("Emotion Predicted Successfully!")
+  except Exception as e:
     print("Error")
+
+print("\n")
